c6fa5d3435d70d388bd78140f9197c7f0058e1cb
Joe Longstreet <joelongstreet@gmail.com>
2014-12-22 20:07:18 -0600

    add one wire digital temperature sensor

--- firmware.ino
+++ firmware.ino
@@ -1,7 +1,14 @@
-int TEMPERATURE_SENSOR = A0;
-int RELAY = D0;
+// This #include statement was automatically added by the Spark IDE.
+#include "OneWire/OneWire.h"
+
+OneWire TEMPERATURE_SENSOR = OneWire(D0);
+uint8_t rom[8];
+uint8_t resp[9];
+
+int RELAY = D1;
+
 int relayState = 0;
-int currentTemperature = 0;
+float currentTemperature = 0.0;
 
 
 int setRelay(String params){
@@ -16,19 +23,44 @@ int setRelay(String params){
 
 void setup()
 {
-  Spark.variable("temperature", &currentTemperature, INT);
+  Spark.variable("temperature", &currentTemperature, STRING);
   Spark.function("set-relay", setRelay);
 }
 
 
 void loop()
 {
-  currentTemperature = analogRead(TEMPERATURE_SENSOR);
+  // Get the ROM address
+  TEMPERATURE_SENSOR.reset();
+  TEMPERATURE_SENSOR.write(0x33);
+  TEMPERATURE_SENSOR.read_bytes(rom, 8);
+
+  // Get the temp
+  TEMPERATURE_SENSOR.reset();
+  TEMPERATURE_SENSOR.write(0x55);
+  TEMPERATURE_SENSOR.write_bytes(rom,8);
+  TEMPERATURE_SENSOR.write(0x44);
+  delay(10);
+  TEMPERATURE_SENSOR.reset();
+  TEMPERATURE_SENSOR.write(0x55);
+  TEMPERATURE_SENSOR.write_bytes(rom, 8);
+  TEMPERATURE_SENSOR.write(0xBE);
+  TEMPERATURE_SENSOR.read_bytes(resp, 9);
+
+  byte MSB = resp[1];
+  byte LSB = resp[0];
+
+  int16_t intTemp   = ((MSB << 8) | LSB); //using two's compliment 16-bit
+  float celsius     = ((double)intTemp)/16.0;
+  float fahrenheit  = (( celsius*9.0)/5.0+32.0);
 
   // publish the relay state and current temperature
   char publishString[64];
-  sprintf(publishString, "{ \"relayState\": \"%u\", \"temperature\": %u }", relayState, currentTemperature);
+  sprintf(publishString, "{ \"relayState\": \"%u\", \"celsius\": %e, , \"fahrenheit\": %e }", relayState, celsius, fahrenheit);
   Spark.publish("temperature", publishString);
+
+  // make available to the spark variable
+  currentTemperature = fahrenheit;
 
   // add a delay to prevent rate limiting
   delay(5000);
